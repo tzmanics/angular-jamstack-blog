a209edbcb9e68014fa06c0c09e1f905a5133aa0c
Tara Z. Manicsic <tzmanics@gmail.com>
2020-07-07 15:01:14 -0400

    adds and runs scully

---
+++ scully.angular-jamstack-blog.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "angular-jamstack-blog",
+  outDir: './dist/static',
+  routes: {
+  }
+};
