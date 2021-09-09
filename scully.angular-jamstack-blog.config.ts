a9c458600b7151820e9c148032544c2ca96a0573
Tara Z. Manicsic <tzmanics@gmail.com>
2021-09-09 01:05:35 -0400

    adds chromium path

--- scully.angular-jamstack-blog.config.ts
+++ scully.angular-jamstack-blog.config.ts
@@ -11,20 +11,5 @@ export const config: ScullyConfig = {
       },
     },
   },
-  puppeteerLaunchOptions: {
-    args: [
-      '--disable-gpu',
-      '--renderer',
-      '--no-sandbox',
-      '--no-service-autorun',
-      '--no-experiments',
-      '--no-default-browser-check',
-      '--disable-dev-shm-usage',
-      '--disable-setuid-sandbox',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
-      '--disable-extensions',
-    ],
-  },
+  puppeteerLaunchOptions: { executablePath: CHROMIUM_PATH },
 };
