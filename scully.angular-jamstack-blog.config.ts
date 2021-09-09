3ab2f109a5faba00d5ea2b4a19720b467580c241
Tara Z. Manicsic <tzmanics@gmail.com>
2021-09-09 00:53:00 -0400

    adds puppeteer config

--- scully.angular-jamstack-blog.config.ts
+++ scully.angular-jamstack-blog.config.ts
@@ -1,14 +1,30 @@
 import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "angular-jamstack-blog",
+  projectRoot: './src',
+  projectName: 'angular-jamstack-blog',
   outDir: './dist/static',
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
       slug: {
-        folder: "./blog"
-      }
+        folder: './blog',
+      },
     },
-  }
+  },
+  puppeteerLaunchOptions: {
+    args: [
+      '--disable-gpu',
+      '--renderer',
+      '--no-sandbox',
+      '--no-service-autorun',
+      '--no-experiments',
+      '--no-default-browser-check',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-extensions',
+    ],
+  },
 };
